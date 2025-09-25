032ef9d412ad29b70571bad0f238255cb95032d6
fay <hisakiryouhei@gmail.com>
2025-09-25 21:10:53 +0700

    mystagram

---
+++ index.js
@@ -0,0 +1,31 @@
+function appendImageElement(keyword, index) {
+    const imgElement = document.createElement('img');
+    imgElement.src = `https://source.unsplash.com/400x225/?${keyword}&sig=${index}`;
+
+    const galleryElement = document.querySelector('.gallery');
+    galleryElement.appendChild(imgElement);
+}
+
+function clearImages() {
+    const galleryElement = document.querySelector('.gallery');
+    galleryElement.innerHTML = '';
+}
+
+function searchPhotos(event) {
+    const keyword = event.target.value;
+
+    if (event.key === 'Enter' && keyword) {
+        clearImages();
+
+        for (let i = 1; i <= 9; i++) {
+            appendImageElement(keyword, i);
+        }
+    }
+}
+
+function run() {
+    const searchInputElement = document.getElementById('search-photos');
+    searchInputElement.addEventListener('keydown', searchPhotos);
+}
+
+run();
